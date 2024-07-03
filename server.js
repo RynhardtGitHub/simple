5d13320ef752aec44a179846c3e303970ad61228
rynhardt <rynhardtluies1@gmail.com>
2024-07-03 09:05:08 +0200

    General commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Start canvas on server to do collision detection
+const { createCanvas } = require('canvas');
+
+// CORS dependencies
 const cors = require('cors');
 app.use(cors());
 
@@ -15,6 +19,7 @@ const MAX_DATA_POINTS = 20;
 let started = false;
 let currentMaze;
 let playerCount = 0;
+let canvaswidth = 400;
 
 // Difficulty progression
 let difficulty = 5;
@@ -55,6 +60,8 @@ io.on('connection', (socket) => {
         averageData.z /= globalDataAccumulator.length;
 
         io.emit('ballPosition', data);
+
+        console.log(`Virtual canvas: ${(ctx.getImageData(0,0,canvaswidth,canvaswidth).data)}`);
     });
 
     // Feed map to players
@@ -65,6 +72,12 @@ io.on('connection', (socket) => {
             console.log(`Emitting maze: ${JSON.stringify(maze)}`)
 
             io.emit('maze', maze);
+
+            const canvas = createCanvas(canvaswidth, canvaswidth);
+            const ctx = canvas.getContext('2d');
+            DrawMaze(maze,ctx,canvaswidth / difficulty);
+
+            // console.log(`Virtual canvas: ${(ctx.getImageData(0,0,canvaswidth,canvaswidth).data)}`);
 
             started = true;
             currentMaze = maze;
@@ -257,4 +270,125 @@ function Maze(Width, Height) {
         endCoord: endCoord,
         difficulty: difficulty
     }
-  }
+}
+
+function DrawMaze(Maze, ctx, cellsize, endSprite = null) {
+
+    // console.log('Entered DrawMaze');
+
+    var map = Maze.map;
+    var cellSize = cellsize;
+
+    // console.log(`cellsize: ${cellsize}`);
+
+    var drawEndMethod;
+    ctx.lineWidth = cellSize / 40;
+
+    // console.log(`linewidth: ${ctx.lineWidth}`);
+  
+    this.redrawMaze = function (size) {
+      cellSize = size;
+      ctx.lineWidth = cellSize / 50;
+      drawMap();
+      drawEndMethod();
+    };
+  
+    function drawCell(xCord, yCord, cell) {
+      var x = xCord * cellSize;
+      var y = yCord * cellSize;
+
+    //   console.log(`drawCell: ${x, y}`);
+  
+      if (cell.n == false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + cellSize, y);
+        ctx.stroke();
+      }
+      if (cell.s === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + cellSize);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke();
+      }
+      if (cell.e === false) {
+        ctx.beginPath();
+        ctx.moveTo(x + cellSize, y);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke();
+      }
+      if (cell.w === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + cellSize);
+        ctx.stroke();
+      }
+    }
+  
+    function drawMap() {
+      for (x = 0; x < map.length; x++) {
+        for (y = 0; y < map[x].length; y++) {
+          drawCell(x, y, map[x][y]);
+        }
+      }
+    }
+  
+    function drawEndFlag() {
+      var coord = Maze.endCoord;
+      var gridSize = 4;
+      var fraction = cellSize / gridSize - 2;
+      var colorSwap = true;
+      for (let y = 0; y < gridSize; y++) {
+        if (gridSize % 2 == 0) {
+          colorSwap = !colorSwap;
+        }
+        for (let x = 0; x < gridSize; x++) {
+          ctx.beginPath();
+          ctx.rect(
+            coord.x * cellSize + x * fraction + 4.5,
+            coord.y * cellSize + y * fraction + 4.5,
+            fraction,
+            fraction
+          );
+          if (colorSwap) {
+            ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+          } else {
+            ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+          }
+          ctx.fill();
+          colorSwap = !colorSwap;
+        }
+      }
+    }
+  
+    function drawEndSprite() {
+      var offsetLeft = cellSize / 50;
+      var offsetRight = cellSize / 25;
+      var coord = Maze.endCoord;
+      ctx.drawImage(
+        endSprite,
+        2,
+        2,
+        endSprite.width,
+        endSprite.height,
+        coord.x * cellSize + offsetLeft,
+        coord.y * cellSize + offsetLeft,
+        cellSize - offsetRight,
+        cellSize - offsetRight
+      );
+    }
+  
+    function clear() {
+      var canvasSize = cellSize * map.length;
+      ctx.clearRect(0, 0, canvasSize, canvasSize);
+    }
+  
+    if (endSprite != null) {
+      drawEndMethod = drawEndSprite;
+    } else {
+      drawEndMethod = drawEndFlag;
+    }
+    clear();
+    drawMap();
+    drawEndMethod();
+}
